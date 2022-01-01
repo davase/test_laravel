5b8019af070ba12ef6e4f827832ccc951f5837d9
余斌 <15021516526@163.com>
2022-01-01 20:14:14 +0800

    feat(laravel项目)：学习路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//调用控制器方法
+Route::get('Controller',[UserController::class,'Test']);
+
+//测试
+Route::get('Test',function (){
+   echo "Hi 路由";
+});
+
+//match 指定访问的方法
+Route::match(['get','post'],'match',function (){
+   echo "match 路由";
+});
+
+//any 任意访问方法
+Route::any('any',function (){
+   echo 'any 路由';
+});
+
+//指定参数
+Route::get('bixuan/{id}',function ($id){
+    echo "可选".$id;
+});
+
+//可选参数
+Route::get('kexuan/{id?}',function ($id="默认值"){
+   echo "可选".$id;
+});
+
+//路由分组
+Route::prefix('admin/admin1/admin2')->group(function (){
+    Route::get('a',function (){
+        return 'a';
+    });
+});
+
+
+
+
+
